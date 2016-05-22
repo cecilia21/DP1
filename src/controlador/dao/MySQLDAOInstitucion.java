a6d209a3fe6dc7abaf8b4189e8fda4c0de0e5897
Raul <Raul@192.168.1.7>
2016-05-22 17:54:32 -0500

    avance

--- src/controlador/dao/MySQLDAOInstitucion.java
+++ src/controlador/dao/MySQLDAOInstitucion.java
@@ -5,6 +5,11 @@
  */
 package controlador.dao;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Driver;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import model.Institucion;
 
@@ -16,7 +21,42 @@ public class MySQLDAOInstitucion implements DAOInstitucion {
 
     @Override
     public void add(Institucion i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+                //Paso 1: Registrar el Driver
+                DriverManager.registerDriver(new Driver());
+                //Paso 2: Obtener la conexión
+                conn = (Connection) DriverManager.getConnection(DBConnection.URL_JDBC_MySQL,
+                                                        DBConnection.user,
+                                                        DBConnection.password);
+                //Paso 3: Preparar la sentencia
+                String sql = "INSERT INTO Institucion "
+                                + "(idDistrito, idLocal, idRegion, nombre,CantidadVotantesRegistrados)"
+                                + "VALUES (?,?,?,?,?)";
+                
+                pstmt = (PreparedStatement) conn.prepareStatement(sql);
+                //pstmt.setInt(1, p.getId());
+                pstmt.setInt(1, i.getIdDistrito());
+                pstmt.setInt(2, i.getIdLocal());
+                pstmt.setInt(3, i.getIdRegion());
+                pstmt.setString(4, i.getNombre());
+                pstmt.setInt(5, i.getCantidadVotantesRegistrados());
+                //Paso 4: Ejecutar la sentencia
+                pstmt.executeUpdate();
+                //Paso 5(opc.): Procesar los resultados			
+        } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        } finally {
+                //Paso 6(OJO): Cerrar la conexión
+                try { if (pstmt!= null) pstmt.close();} 
+                        catch (Exception e){e.printStackTrace();};
+                try { if (conn!= null) conn.close();} 
+                        catch (Exception e){e.printStackTrace();};						
+        }       
+        
     }
 
     @Override
