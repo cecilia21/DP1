017c0b3ed2bb2b370dd067e6571826326bae35aa
ramonbv <simon81265@gmail.com>
2016-06-05 12:44:25 -0500

    Merge origin/master
    Conflicts:
    	src/controlador/dao/DBConnection.java
    	src/grupob/TipoProceso.java

--- src/algoritmos/Algoritmo_Huellas.java
+++ src/algoritmos/Algoritmo_Huellas.java
@@ -285,88 +285,88 @@ public class Algoritmo_Huellas {
         return (1-por);
     }
     
-    private static BufferedImage readImage(File file) throws IOException
-    {
-        return readImage(new FileInputStream(file));
-    }
-
-    
-    private static BufferedImage readImage(InputStream stream) throws IOException
-    {
-        Iterator<ImageReader> imageReaders = 
-            ImageIO.getImageReadersBySuffix("jpg");
-        ImageReader imageReader = imageReaders.next();
-        ImageInputStream iis = 
-            ImageIO.createImageInputStream(stream);
-        imageReader.setInput(iis, true, true);
-        Raster raster = imageReader.readRaster(0, null);
-        int w = raster.getWidth();
-        int h = raster.getHeight();
-
-        BufferedImage result = 
-            new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-        int rgb[] = new int[3];
-        int pixel[] = new int[3];
-        for (int x=0; x<w; x++)
-        {
-            for (int y=0; y<h; y++)
-            {
-                raster.getPixel(x, y, pixel);
-                int Y = pixel[0];
-                int CR = pixel[1];
-                int CB = pixel[2];
-                toRGB(Y, CB, CR, rgb);
-                int r = rgb[0];
-                int g = rgb[1];
-                int b = rgb[2];
-                int bgr = 
-                    ((b & 0xFF) << 16) | 
-                    ((g & 0xFF) <<  8) | 
-                     (r & 0xFF);
-                result.setRGB(x, y, bgr);
-            }
-        }
-        return result;
-    }
-
-    private static void toRGB(int y, int cb, int cr, int rgb[])
-    {
-        float Y = y / 255.0f;
-        float Cb = (cb-128) / 255.0f;
-        float Cr = (cr-128) / 255.0f;
-
-        float R = Y + 1.4f * Cr;
-        float G = Y -0.343f * Cb - 0.711f * Cr;
-        float B = Y + 1.765f * Cb;
-
-        R = Math.min(1.0f, Math.max(0.0f, R));
-        G = Math.min(1.0f, Math.max(0.0f, G));
-        B = Math.min(1.0f, Math.max(0.0f, B));
-
-        int r = (int)(R * 255);
-        int g = (int)(G * 255);
-        int b = (int)(B * 255);
-
-        rgb[0] = r;
-        rgb[1] = g;
-        rgb[2] = b;
-    }
+//    private static BufferedImage readImage(File file) throws IOException
+//    {
+//        return readImage(new FileInputStream(file));
+//    }
+//
+//    
+//    private static BufferedImage readImage(InputStream stream) throws IOException
+//    {
+//        Iterator<ImageReader> imageReaders = 
+//            ImageIO.getImageReadersBySuffix("jpg");
+//        ImageReader imageReader = imageReaders.next();
+//        ImageInputStream iis = 
+//            ImageIO.createImageInputStream(stream);
+//        imageReader.setInput(iis, true, true);
+//        Raster raster = imageReader.readRaster(0, null);
+//        int w = raster.getWidth();
+//        int h = raster.getHeight();
+//
+//        BufferedImage result = 
+//            new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+//        int rgb[] = new int[3];
+//        int pixel[] = new int[3];
+//        for (int x=0; x<w; x++)
+//        {
+//            for (int y=0; y<h; y++)
+//            {
+//                raster.getPixel(x, y, pixel);
+//                int Y = pixel[0];
+//                int CR = pixel[1];
+//                int CB = pixel[2];
+//                toRGB(Y, CB, CR, rgb);
+//                int r = rgb[0];
+//                int g = rgb[1];
+//                int b = rgb[2];
+//                int bgr = 
+//                    ((b & 0xFF) << 16) | 
+//                    ((g & 0xFF) <<  8) | 
+//                     (r & 0xFF);
+//                result.setRGB(x, y, bgr);
+//            }
+//        }
+//        return result;
+//    }
+//
+//    private static void toRGB(int y, int cb, int cr, int rgb[])
+//    {
+//        float Y = y / 255.0f;
+//        float Cb = (cb-128) / 255.0f;
+//        float Cr = (cr-128) / 255.0f;
+//
+//        float R = Y + 1.4f * Cr;
+//        float G = Y -0.343f * Cb - 0.711f * Cr;
+//        float B = Y + 1.765f * Cb;
+//
+//        R = Math.min(1.0f, Math.max(0.0f, R));
+//        G = Math.min(1.0f, Math.max(0.0f, G));
+//        B = Math.min(1.0f, Math.max(0.0f, B));
+//
+//        int r = (int)(R * 255);
+//        int g = (int)(G * 255);
+//        int b = (int)(B * 255);
+//
+//        rgb[0] = r;
+//        rgb[1] = g;
+//        rgb[2] = b;
+//    }
     
     public double VerificaHuella(String dni,BufferedImage huellaC){
         double por=-1;
         BufferedImage ot=null;
         BufferedImage tt=null;
-       try {
-//           ot = readImage(new File("F:/Huellas3/huellasna1.jpg"));
-           tt = readImage(new File("F:/Huellas3/huellasna2.jpg"));
-       } catch (IOException ex) {
-           Logger.getLogger(Algoritmo_Huellas.class.getName()).log(Level.SEVERE, null, ex);
-       }
+//       try {
+////           ot = readImage(new File("F:/Huellas3/huellasna1.jpg"));
+//           tt = readImage(new File("F:/Huellas3/huellasna2.jpg"));
+//       } catch (IOException ex) {
+//           Logger.getLogger(Algoritmo_Huellas.class.getName()).log(Level.SEVERE, null, ex);
+//       }
         BufferedImage huellaO=buscaHuella(dni);
 //        huellaO=ot;
         if(huellaO!=null){
 //            huellaO=ot;
-            huellaC=tt;
+//            huellaC=tt;
             int heightO=huellaO.getHeight();
             int heightC=huellaC.getHeight();
             int widthO=huellaO.getWidth();
