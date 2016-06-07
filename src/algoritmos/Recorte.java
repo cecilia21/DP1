1e4ed26c8fb8a15c5dc7b7345e83c727ec3b237d
Sephirihos <sephirihos18@gmail.com>
2016-06-07 18:51:13 -0500

    cambios

--- src/algoritmos/Recorte.java
+++ src/algoritmos/Recorte.java
@@ -220,15 +220,5 @@ public class Recorte {
         }
     }
     
-    
-       private static BufferedImage resizeImage(BufferedImage originalImage, int type,
-                                         Integer img_width, Integer img_height)
-{
-    BufferedImage resizedImage = new BufferedImage(img_width, img_height, type);
-    Graphics2D g = resizedImage.createGraphics();
-    g.drawImage(originalImage, 0, 0, img_width, img_height, null);
-    g.dispose();
-
-    return resizedImage;
-}
+   
 }
